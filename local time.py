b2c583d12a79afc9678efe2f55f84921533ede54
Abdullahi Yusuf <45642669+lovowAb@users.noreply.github.com>
2018-12-16 19:36:07 +0200

    uploaded v1.0
    its shows the calendar and the time

--- local time.py
+++ local time.py
@@ -1,1 +1,13 @@
-print("local time")
+import time
+import calendar
+
+localtime = time.localtime(time.time())
+print ("Local current time :", localtime)
+
+
+localtime = time.asctime(time.localtime(time.time()))
+print ("Local current time :", localtime)
+
+cal = calendar.month(2018, 12)
+print ("Here is the calendar:")
+print (cal)
